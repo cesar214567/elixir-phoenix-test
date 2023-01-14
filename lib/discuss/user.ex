a4d43af1f58a5b17694b20b57252c904c39ebdec
cesar214567 <camgcamg11@gmail.com>
2023-01-14 09:22:29 -0500

    Oauth done with facebook

---
+++ lib/discuss/user.ex
@@ -0,0 +1,27 @@
+defmodule Discuss.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "users" do
+    field :email, :string
+    field :provider, :string
+    field :token, :string
+
+    timestamps()
+  end
+
+  @spec changeset(
+          {map, map}
+          | %{
+              :__struct__ => atom | %{:__changeset__ => map, optional(any) => any},
+              optional(atom) => any
+            },
+          :invalid | %{optional(:__struct__) => none, optional(atom | binary) => any}
+        ) :: Ecto.Changeset.t()
+  @doc false
+  def changeset(user, attrs) do
+    user
+    |> cast(attrs, [:email, :provider, :token])
+    |> validate_required([:email, :provider, :token])
+  end
+end
